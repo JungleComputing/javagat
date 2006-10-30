6f28f329e69cc333d1ccbb935acd62dadf5e1ace
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2006-10-30 14:27:13 +0000

    Fixed some typos

--- engine/src/org/gridlab/gat/resources/Job.java
+++ engine/src/org/gridlab/gat/resources/Job.java
@@ -136,33 +136,34 @@ public abstract class Job implements Monitorable, Advertisable {
      * </ul>
      * <p>
      * <em>state</em> The key state corresponds to a java.lang.String
-     * value which is the name of the state the job is in. The sate strings are literally
-     * the same as the name of the constants used, e.g., "RUNNING", or "STOPPED".
+     * value which is the name of the state the job is in. The state strings
+     * are literally the same as the name of the constants used,
+     * e.g., "RUNNING", or "STOPPED".
      * <p>
      * <em>hostname</em> The key hostname corresponds to a java.lang.String
      * value which is the name of the host on which the physical job is running,
-     * if is in the Running state. If the associated is not in the Running
-     * state, the value is null.
+     * if the job is in the Running state. If the associated job is not in the
+     * Running state, the value is null.
      * <p>
      * <em>submissiontime</em> The key submissiontime corresponds to a long
      * value which is the number of milliseconds after January 1, 1970, 00:00:00
      * GMT when the associated physical job was submitted. This value is null
-     * for a in the Constructed state otherwise it is not null.
+     * for a job in the Constructed state otherwise it is not null.
      * <p>
      * <em>starttime</em> The key starttime corresponds to a long value which
      * is the number of milliseconds after January 1, 1970, 00:00:00 GMT when
-     * the associated physical job was started. This value is null for a in the
-     * Submitted or Constructed states otherwise it is not null.
+     * the associated physical job was started. This value is null for a job in
+     * the Submitted or Constructed states otherwise it is not null.
      * <p>
-     * <em>stoptime<em>
+     * <em>stoptime</em>
      * The key stoptime corresponds to a long value which is the
      * number of milliseconds after January 1, 1970, 00:00:00 GMT when the
      * associated physical job stopped. This value is not null for a
-     *  in the Stopped state otherwise it is null.
+     * job in the Stopped state otherwise it is null.
      * <p>
-     * <em>postStageError<em>
-     * This key is present in the map if the application did run, but one or more files 
-     * could not be post staged.
+     * <em>postStageError</em>
+     * This key is present in the map if the application did run, but one or
+     * more files could not be post staged.
      * The data value attached to this key is the exception that occurred while 
      * poststageing.
      *
@@ -170,7 +171,7 @@ public abstract class Job implements Monitorable, Advertisable {
      * pairs returned in this java.util.Map as the need develops.
      *
      * @return An instance of the class java.util.Map which presents
-     * information about the associated .
+     * information about the associated job.
      * @throws GATInvocationException Thrown upon problems
      * accessing the remote instance
      */
