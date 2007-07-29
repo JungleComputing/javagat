be26b887b1c4878682b8c62af512b7d43b3de2aa
Anna Chmielowiec <acc200@few.vu.nl>
2007-07-29 21:13:01 +0000

    updating GLiteResourceBrokerAdaptor and GLiteJob

--- adaptors/src/org/gridlab/gat/resources/cpi/glite/GLiteJob.java
+++ adaptors/src/org/gridlab/gat/resources/cpi/glite/GLiteJob.java
@@ -3,10 +3,197 @@
  */
 package org.gridlab.gat.resources.cpi.glite;
 
+import java.util.HashMap;
+
+import org.glite.wms.wmproxy.AuthorizationFaultException;
+import org.glite.wms.wmproxy.AuthenticationFaultException;
+import org.glite.wms.wmproxy.InvalidArgumentFaultException;
+import org.glite.wms.wmproxy.JobIdStructType;
+import org.glite.wms.wmproxy.JobUnknownFaultException;
+import org.glite.wms.wmproxy.OperationNotAllowedFaultException;
+import org.glite.wms.wmproxy.ServiceException;
+import org.glite.wms.wmproxy.StringAndLongList;
+import org.glite.wms.wmproxy.WMProxyAPI;
+
+import org.gridlab.gat.GATContext;
+import org.gridlab.gat.GATInvocationException;
+import org.gridlab.gat.Preferences;
+import org.gridlab.gat.engine.GATEngine;
+import org.gridlab.gat.monitoring.Metric;
+import org.gridlab.gat.monitoring.MetricDefinition;
+import org.gridlab.gat.resources.Job;
+import org.gridlab.gat.resources.JobDescription;
 import org.gridlab.gat.resources.cpi.JobCpi;
+import org.gridlab.gat.resources.cpi.Sandbox;
+
+/**
+ * This thread actively checks if the output files are ready to copy. If not the
+ * exception is caught. This is not the most elegant way but it is not possible
+ * to poll the state of gLite job right now.
+ * 
+ * @author anna
+ * 
+ */
+class GLiteJobPoller extends Thread {
+	private GLiteJob job;
+
+	private boolean die = false;
+
+	GLiteJobPoller(GLiteJob j) {
+		this.job = j;
+		setDaemon(true);
+	}
+
+	public void run() {
+		while (true) {
+			if (job.getState() == Job.STOPPED)
+				return;
+			if (job.getState() == Job.SUBMISSION_ERROR)
+				return;
+			job.tryGetOutputFiles();
+			if (job.getState() == Job.STOPPED)
+				return;
+			if (job.getState() == Job.SUBMISSION_ERROR)
+				return;
+
+			synchronized (this) {
+				try {
+					wait(20 * 1000);
+				} catch (Exception e) {
+					// Ignore
+				}
+				if (die) {
+					if (GATEngine.DEBUG) {
+						System.err.println("gLite job poller killed");
+					}
+					return;
+				}
+			}
+		}
+	}
+
+	synchronized void die() {
+		die = true;
+		notifyAll();
+	}
+}
+
 /**
  * @author anna
  */
 public class GLiteJob extends JobCpi {
 
+	private WMProxyAPI client;
+
+	private String jobID;
+
+	private JobIdStructType jobStruct;
+
+	private long startTime;
+
+	private MetricDefinition statusMetricDefinition;
+
+	private Metric statusMetric;
+
+	private GLiteJobPoller poller;
+
+	public GLiteJob(GATContext gatContext, Preferences preferences,
+			JobDescription jobDescription, WMProxyAPI client,
+			long startTime, JobIdStructType jobStruct) {
+		// TODO: there should be sandbox instead of null!!!
+		super(gatContext, preferences, jobDescription, null);
+		this.client = client;
+		this.startTime = startTime;
+		this.jobStruct = jobStruct;
+		this.jobID = jobStruct.getId();
+		// it is not yet possible to acquire job state
+		// we just check if we can already copy output files
+		state = Job.UNKNOWN;
+		// Tell the engine that we provide job.status events
+		/*
+		HashMap returnDef = new HashMap();
+		returnDef.put("status", String.class);
+		statusMetricDefinition = new MetricDefinition("job.status",
+				MetricDefinition.DISCRETE, "String", null, null, returnDef);
+		GATEngine.registerMetric(this, "getJobStatus", statusMetricDefinition);
+		statusMetric = statusMetricDefinition.createMetric(null);
+		*/
+		poller = new GLiteJobPoller(this);
+		poller.start();
+	}
+
+	public String getJobID() throws GATInvocationException {
+		return jobID;
+	}
+
+	/* we use this method to check if the job has already ended */
+	void tryGetOutputFiles() {
+		try {
+			StringAndLongList list = client.getOutputFileList(jobID);
+			setState(POST_STAGING);
+			// TODO: file copying
+			setState(STOPPED);
+		} catch (AuthorizationFaultException afe) {
+			// the client is not authorized to perform this operation
+			setState(SUBMISSION_ERROR);
+		} catch (AuthenticationFaultException aufe) {
+			// a generic authentication problem occurred
+			setState(SUBMISSION_ERROR);
+		} catch (JobUnknownFaultException jufe) {
+			// the given job has not been registered to the system
+			setState(SUBMISSION_ERROR);
+		} catch (InvalidArgumentFaultException iafe) {
+			// the given job Id is not valid
+			setState(SUBMISSION_ERROR);
+		} catch (OperationNotAllowedFaultException onafe) {
+			// the current job status does not allow requested operation
+			// we don't know if the job is scheduled or already running
+			setState(UNKNOWN);
+		} catch (ServiceException se) {
+			// other error occured during the execution of the remote method
+			// call to the WMProxy server
+			setState(SUBMISSION_ERROR);
+		} catch (Exception e) {
+			// some other exception caught; lets wait
+			setState(UNKNOWN);
+		}
+	}
+
+	// TODO: synchronization with tryGetOutputFiles needed!!!
+	public void stop() throws GATInvocationException {
+		try {
+			client.jobCancel(jobID);
+			setState(STOPPED);
+		} catch (AuthorizationFaultException afe) {
+			throw new GATInvocationException(
+					"The client is not authorized to perform this operation",
+					afe);
+		} catch (AuthenticationFaultException aufe) {
+			throw new GATInvocationException(
+					"A generic authentication problem occurred", aufe);
+		} catch (JobUnknownFaultException jufe) {
+			throw new GATInvocationException(
+					"The given job has not been registered to the system", jufe);
+		} catch (InvalidArgumentFaultException iafe) {
+			throw new GATInvocationException("The given job Id is not valid "
+					+ jobID, iafe);
+		} catch (OperationNotAllowedFaultException onafe) {
+			throw new GATInvocationException(
+					"The current job status does not allow requested operation",
+					onafe);
+		} catch (ServiceException se) {
+			throw new GATInvocationException(
+					"Unknown error occured during the execution of the remote method call to the WMProxy server",
+					se);
+		} catch (Exception e) {
+			throw new GATInvocationException(
+					"Unknown error occured during the execution of the remote method call to the WMProxy server",
+					e);
+		}
+
+	}
+
+	protected synchronized void setState(int newState) {
+		state = newState;
+	}
 }
--- adaptors/src/org/gridlab/gat/resources/cpi/glite/GLiteResourceBrokerAdaptor.java
+++ adaptors/src/org/gridlab/gat/resources/cpi/glite/GLiteResourceBrokerAdaptor.java
@@ -3,19 +3,33 @@
  */
 package org.gridlab.gat.resources.cpi.glite;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
 
 import org.glite.jdl.JobAd;
+import org.glite.jdl.JobAdException;
+import org.glite.wms.wmproxy.AuthorizationFaultException;
+import org.glite.wms.wmproxy.AuthenticationFaultException;
+import org.glite.wms.wmproxy.InvalidArgumentFaultException;
+import org.glite.wms.wmproxy.JobIdStructType;
+import org.glite.wms.wmproxy.NoSuitableResourcesFaultException;
+import org.glite.wms.wmproxy.ServiceException;
 import org.glite.wms.wmproxy.StringAndLongList;
 import org.glite.wms.wmproxy.StringAndLongType;
 import org.glite.wms.wmproxy.WMProxyAPI;
 
 import org.gridlab.gat.GATContext;
 import org.gridlab.gat.GATInvocationException;
+import org.gridlab.gat.GATObjectCreationException;
 import org.gridlab.gat.Preferences;
+import org.gridlab.gat.URI;
+import org.gridlab.gat.io.File;
 import org.gridlab.gat.resources.Job;
 import org.gridlab.gat.resources.JobDescription;
 import org.gridlab.gat.resources.ResourceDescription;
+import org.gridlab.gat.resources.SoftwareDescription;
 import org.gridlab.gat.resources.cpi.ResourceBrokerCpi;
 
 /**
@@ -24,66 +38,254 @@ import org.gridlab.gat.resources.cpi.ResourceBrokerCpi;
 
 public class GLiteResourceBrokerAdaptor extends ResourceBrokerCpi {
 
+	private String delegationId;
+
 	public GLiteResourceBrokerAdaptor(GATContext gatContext,
-            Preferences preferences) throws GATObjectCreationException {
-        super(gatContext, preferences);
+			Preferences preferences) throws GATObjectCreationException {
+		super(gatContext, preferences);
 	}
-	
-	private WMProxyAPI connectToWMProxy() {
+
+	private String getDelegationId() {
+		if (delegationId == null)
+			delegationId = "delegationId" + Math.round(Math.random() * 10000.0);
+		return delegationId;
+	}
+
+	/* connect to WMProxy */
+	private WMProxyAPI connectToWMProxy(String delegationId)
+			throws GATInvocationException {
 		WMProxyAPI client = null;
 		try {
 			// TODO: where to find host, port and proxy file?
 			String host = "mu12.matrix.sara.nl";
-		    String port = "7443";
-		    String proxyFile = "/tmp/x509up_u1380";
-		    
-			client = new WMProxyAPI("https://" + host + ":" + port + "/glite_wms_wmproxy_server" , proxyFile);
+			String port = "7443";
+			String proxyFile = "/tmp/x509up_u1380";
+
+			client = new WMProxyAPI("https://" + host + ":" + port
+					+ "/glite_wms_wmproxy_server", proxyFile);
 			// delegation of user credentials
-			// TODO: change id
-			String delegationId = "anna";
 			String proxy = client.grstGetProxyReq(delegationId);
 			client.grstPutProxy(delegationId, proxy);
 		} catch (Exception e) {
-			throw GATInvocationException("Unable to connect to WMProxy service", e);
+			throw new GATInvocationException(
+					"Unable to connect to WMProxy service", e);
 		}
 		return client;
 	}
-	
-	private JobAd createJobDescription(JobDescription description) {
-		JobAd jobAd;
-		
+
+	/* check correctness of jobAd */
+	private void checkJobAd(JobAd jobAd) throws GATInvocationException {
+		try {
+			jobAd.checkAll();
+		} catch (IllegalArgumentException iae) {
+			throw new GATInvocationException(
+					"Wrong type/format/value for an attribute in JobDescription",
+					iae);
+		} catch (JobAdException jae) {
+			throw new GATInvocationException(
+					"One or more values do not match with semantic rule", jae);
+		}
+	}
+
+	private String getJobType(SoftwareDescription sd, String type) {
+		// now only "Normal" type of job is supported
+		String jobType = "Normal";
+		return jobType;
+	}
+
+	private String getExecutable(JobDescription description)
+			throws GATInvocationException {
+		URI location = getLocationURI(description);
+		return location.getPath();
+	}
+
+	private void setEnvironment(JobAd jobAd, SoftwareDescription sd)
+			throws Exception {
+		HashMap environment = (HashMap) sd.getEnvironment();
+		if (environment == null)
+			return;
+		Iterator it = environment.keySet().iterator();
+		while (it.hasNext()) {
+			String name = (String) it.next();
+			String value = (String) environment.get(name);
+			jobAd.addAttribute("Environment", name + "=" + value);
+		}
+	}
+
+	private void setStdInputOutputError(JobAd jobAd, SoftwareDescription sd)
+			throws Exception {
+		File stdin = sd.getStdin();
+		File stdout = sd.getStdin();
+		File stderr = sd.getStdin();
+		// i have some doubts whether to use getName or getPath here
+		if (stdin != null)
+			jobAd.setAttribute("StdInput", stdin.getPath());
+		if (stdout != null)
+			jobAd.setAttribute("StdOutput", stdout.getPath());
+		if (stderr != null)
+			jobAd.setAttribute("StdError", stderr.getPath());
+	}
+
+	private void setInputSandbox(JobAd jobAd, SoftwareDescription sd)
+			throws Exception {
+		HashMap files = (HashMap) sd.getPreStaged();
+		if (files == null)
+			return;
+		Iterator it = files.keySet().iterator();
+		while (it.hasNext()) {
+			File src = (File) it.next();
+			File dest = (File) files.get(src);
+			if (dest == null)
+				jobAd.addAttribute("InputSandbox", src.getPath());
+			else
+				jobAd.addAttribute("InputSandbox", dest.getName());
+		}
+		// all files are put into one directory (without tree structure)
+		// maybe names are enough?
+	}
+
+	private void setOutputSandbox(JobAd jobAd, SoftwareDescription sd)
+			throws Exception {
+		HashMap files = (HashMap) sd.getPostStaged();
+		if (files == null)
+			return;
+		Iterator it = files.keySet().iterator();
+		while (it.hasNext()) {
+			File src = (File) it.next();
+			jobAd.addAttribute("OutputSandbox", src.getPath());
+		}
+	}
+
+	/* create jdl description of the job */
+	/* for now only softwareDescription is supported */
+	private JobAd createJobDescription(JobDescription description)
+			throws GATInvocationException {
+		SoftwareDescription sd = description.getSoftwareDescription();
+		if (sd == null)
+			throw new GATInvocationException(
+					"The job description does not contain a software description");
+		JobAd jobAd = new JobAd();
+		try {
+			// Type options: "Job", "DAG", "Collection" (only Job is now
+			// supported)
+			jobAd.setAttribute("Type", "Job");
+			// JobType options: "Normal", "Interactive", "MPICH",
+			// "Partitinable", "Checkpointable", "Parametric"
+			// "Partitinable" and "Parametric" options are not available for
+			// DAGs and Collections
+			jobAd.setAttribute("JobType", getJobType(sd, "Job"));
+			// Executable (here only the path not the URI is needed and correct)
+			// if executable is not already on the remote CE WN it should be in
+			// prestage files
+			jobAd.setAttribute("Executable", getExecutable(description));
+			// Arguments
+			jobAd.setAttribute("Arguments", getArguments(description));
+			// Environment
+			setEnvironment(jobAd, sd);
+			// StdInput, StdOutput, StdError
+			setStdInputOutputError(jobAd, sd);
+			// InputSandbox
+			setInputSandbox(jobAd, sd);
+			// OutputSandbox
+			setOutputSandbox(jobAd, sd);
+			// parsing
+			// directory (String): working directory - not supported
+			// count (Integer/String): number of executables to run. - not
+			// supported
+			// hostCount (Integer/String): number of hosts to distribute on. -
+			// not supported
+			// maxTime (Long/String): maximal time in minutes. - not supported
+			// TODO: maxWallTime (Long/String): maximal WALL time in minutes.
+			// TODO: maxCPUTime (Long/String): maximal CPU time in minutes.
+			// TODO: queue (String): target queue name.
+			// project (String): project account to use. - not supported
+			// TODO: minMemory (Integer/String): minimal required memory in MB.
+			// maxMemory (Integer/String): maximal required memory in MB. - not
+			// supported (why use it?)
+			// TODO: (check if possible) saveState (Boolean/String): keep job
+			// data persistent for restart.
+			// TODO: (check if possible) restart=ID (String): restart job with
+			// given ID.
+		} catch (Exception e) {
+			throw new GATInvocationException(
+					"Exception while parsing softwareDescription", e);
+		}
+		checkJobAd(jobAd);
 		return jobAd;
 	}
-	
-	private JobAd createJobDescription(ResourceDescription resourceDescription) {
-		JobAd jobAd;
-		
+
+	private JobAd createJobDescription(ResourceDescription resourceDescription)
+			throws GATInvocationException {
+		JobAd jobAd = new JobAd();
+		// TODO: creating jdl
+		checkJobAd(jobAd);
 		return jobAd;
 	}
-	
+
 	/*
-     * (non-Javadoc)
-     *
-     * @see org.gridlab.gat.resources.ResourceBroker#findResources(org.gridlab.gat.resources.ResourceDescription)
-     */
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gridlab.gat.resources.ResourceBroker#findResources(org.gridlab.gat.resources.ResourceDescription)
+	 */
 	public List findResources(ResourceDescription resourceDescription)
 			throws GATInvocationException {
-		//throw new UnsupportedOperationException("Not implemented");
-		WMProxyAPI client = connectToWMProxy();
-		List resources = null;
+		// throw new UnsupportedOperationException("Not implemented");
+		WMProxyAPI client = connectToWMProxy(delegationId);
+		ArrayList resources = null;
 		JobAd jobAd = createJobDescription(resourceDescription);
 		String jdlString = jobAd.toString();
-		//CE Ids satisfying the job Requirements specified in the JDL, ordered according to the decreasing Rank
-		StringAndLongList matchingResources = proxyAPI.jobListMatch(jdlString, delegationId);
+		// CE Ids satisfying the job Requirements specified in the JDL, ordered
+		// according to the decreasing Rank
+		StringAndLongList matchingResources = null;
+		try {
+			matchingResources = client.jobListMatch(jdlString, delegationId);
+		} catch (AuthorizationFaultException afe) {
+			throw new GATInvocationException(
+					"The client is not authorized to perform this operation",
+					afe);
+		} catch (AuthenticationFaultException aufe) {
+			throw new GATInvocationException(
+					"A generic authentication problem occurred", aufe);
+		} catch (NoSuitableResourcesFaultException nsrfe) {
+			// no suitable resources matching job requirements have been found.
+			return resources;
+		} catch (InvalidArgumentFaultException iafe) {
+			throw new GATInvocationException(
+					"The given job JDL expression is not valid", iafe);
+		} catch (ServiceException se) {
+			throw new GATInvocationException(
+					"Unknown error occured during the execution of the remote method call to the WMProxy server",
+					se);
+		} catch (Exception e) {
+			throw new GATInvocationException(
+					"Unknown error occured during the execution of the remote method call to the WMProxy server",
+					e);
+		}
 		if (matchingResources != null) {
-			resources = new List();
-			StringAndLongType[] matchingResourcesList = matchingResources.getFile();
+			resources = new ArrayList();
+			StringAndLongType[] matchingResourcesList = matchingResources
+					.getFile();
 			for (int i = 0; i < matchingResourcesList.length; i++) {
 				resources.add(matchingResourcesList[i].getName());
 			}
 		}
-		//CE Ids satisfying the job Requirements specified in the JDL
+		// CE Ids satisfying the job Requirements specified in the JDL
 		return resources;
+	}
+
+	public boolean isDryRun(JobDescription description) throws GATInvocationException {
+		SoftwareDescription sd = description.getSoftwareDescription();
+		if (sd == null)
+			throw new GATInvocationException(
+					"The job description does not contain a software description");
+		HashMap attributes = (HashMap) sd.getAttributes();
+		if (attributes == null)
+			return false;
+		Boolean dryRun = (Boolean) attributes.get("dryRun");
+		if (dryRun != null)
+			return dryRun.booleanValue();
+		else
+			return false;
 	}
 
 	/*
@@ -93,8 +295,35 @@ public class GLiteResourceBrokerAdaptor extends ResourceBrokerCpi {
 	 */
 	public Job submitJob(JobDescription description)
 			throws GATInvocationException {
-		throw new UnsupportedOperationException("Not implemented");
-		WMProxyAPI client = connectToWMProxy();
+		// throw new UnsupportedOperationException("Not implemented");
+		boolean hasPrestageFiles = false;
+		String delegationId = getDelegationId();
+		WMProxyAPI client = connectToWMProxy(delegationId);
+		JobAd jobAd = createJobDescription(description);
+		long startTime = System.currentTimeMillis();
+		if (isDryRun(description)) {
+			GLiteJob job = new GLiteJob(gatContext, preferences, description,
+					client, startTime, null);
+			job.setState(Job.STOPPED);
+			return job;
+		}
+		String jdlString = jobAd.toString();
+
+		JobIdStructType jobId = null;
+		try {
+			if (hasPrestageFiles) {
+				jobId = client.jobRegister(jdlString, delegationId);
+				// TODO: copying files to CE WN
+				client.jobStart(jobId.getId());
+			} else {
+				jobId = client.jobSubmit(jdlString, delegationId);
+			}
+		} catch (Exception e) {
+			throw new GATInvocationException("Exception at job subbmission", e);
+		}
+		GLiteJob job = new GLiteJob(gatContext, preferences, description,
+				client, startTime, jobId);
+		return job;
 	}
-	
+
 }
