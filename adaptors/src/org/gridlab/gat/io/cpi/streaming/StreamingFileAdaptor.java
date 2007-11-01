6c87fc4e4e88fc9d11fd773d1514091b13063f9b
Roelof Kemp <r.kemp@cs.vu.nl>
2007-11-01 10:21:45 +0000

    roelof: added exists method to streamingFileAdaptor

--- adaptors/src/org/gridlab/gat/io/cpi/streaming/StreamingFileAdaptor.java
+++ adaptors/src/org/gridlab/gat/io/cpi/streaming/StreamingFileAdaptor.java
@@ -5,12 +5,14 @@ package org.gridlab.gat.io.cpi.streaming;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import org.gridlab.gat.GAT;
 import org.gridlab.gat.GATContext;
 import org.gridlab.gat.GATInvocationException;
+import org.gridlab.gat.GATObjectCreationException;
 import org.gridlab.gat.Preferences;
 import org.gridlab.gat.URI;
 import org.gridlab.gat.io.File;
@@ -20,37 +22,54 @@ import org.gridlab.gat.io.cpi.FileCpi;
 
 @SuppressWarnings("serial")
 public class StreamingFileAdaptor extends FileCpi {
-    
-	public StreamingFileAdaptor(GATContext gatContext,
-        Preferences preferences, URI location) {
+
+    public StreamingFileAdaptor(GATContext gatContext, Preferences preferences,
+            URI location) {
         super(gatContext, preferences, location);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.gridlab.gat.io.cpi.FileCpi#copy(org.gridlab.gat.URI)
      */
     public void copy(URI dest) throws GATInvocationException {
         try {
-        	FileInputStream in = GAT.createFileInputStream(gatContext, location);
+            FileInputStream in = GAT
+                    .createFileInputStream(gatContext, location);
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader inBuffer = new BufferedReader(reader);
-        	File dstFile = GAT.createFile(gatContext, dest);
-            FileOutputStream out = GAT.createFileOutputStream(gatContext, dstFile);
+            File dstFile = GAT.createFile(gatContext, dest);
+            FileOutputStream out = GAT.createFileOutputStream(gatContext,
+                    dstFile);
             OutputStreamWriter writer = new OutputStreamWriter(out);
             BufferedWriter outBuffer = new BufferedWriter(writer);
-            while(true) {
+            while (true) {
                 String line = inBuffer.readLine();
                 if (line == null) {
-                	break;
+                    break;
                 }
-            	outBuffer.write(line);
-            	outBuffer.newLine();
+                outBuffer.write(line);
+                outBuffer.newLine();
             }
             in.close();
             outBuffer.flush();
             out.close();
         } catch (Exception e) {
             throw new GATInvocationException("streaming copy", e);
-        } 
+        }
+    }
+
+    // this method does *not* work for empty files
+    public boolean exists() {
+        try {
+            FileInputStream in = GAT
+                    .createFileInputStream(gatContext, location);
+            return (in.read() != -1);
+        } catch (GATObjectCreationException e) {
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
