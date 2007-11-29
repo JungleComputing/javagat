72e74a660f07a448e34946e64bd379558ef6325d
Maciej Wojciechowski <maciekwojciechowski@gmail.com>
2007-11-29 13:20:02 +0000

    GridSAM based on LocalResourceAdaptor. Managed to do new, working resource broker.

--- tests/src/resources/SubmitJobGridSAM.java
+++ tests/src/resources/SubmitJobGridSAM.java
@@ -1,39 +1,36 @@
 package resources;
 
-import java.util.Hashtable;
-
 import org.gridlab.gat.GAT;
 import org.gridlab.gat.GATContext;
 import org.gridlab.gat.Preferences;
-import org.gridlab.gat.resources.HardwareResourceDescription;
+import org.gridlab.gat.io.File;
 import org.gridlab.gat.resources.Job;
 import org.gridlab.gat.resources.JobDescription;
 import org.gridlab.gat.resources.ResourceBroker;
-import org.gridlab.gat.resources.ResourceDescription;
 import org.gridlab.gat.resources.SoftwareDescription;
 
 public class SubmitJobGridSAM {
-
     public static void main(String[] args) throws Exception {
-        new SubmitJobGridSAM().start(args);
-    }
-    
-    public void start(String args[]) throws Exception {
         GATContext context = new GATContext();
+        
         Preferences prefs = new Preferences();
-        prefs.put("ResourceBroker.adaptor.name", "Globus");
-        
-        SoftwareDescription sd = new SoftwareDescription();
-        sd.setLocation("xxx");
-        
-        Hashtable<String, Object> hardwareAttributes = new Hashtable<String, Object>();
 
-        ResourceDescription rd = new HardwareResourceDescription(
-            hardwareAttributes);
-        
-        JobDescription jd = new JobDescription(sd, rd);
-        
+        prefs.put("ResourceBroker.adaptor.name", "GridSAM");
+        SoftwareDescription sd = new SoftwareDescription();
+        sd.setLocation("file:////bin/hostname");
+
+        File stdout = GAT.createFile(context, "hostname.txt");
+        sd.setStdout(stdout);
+
+        JobDescription jd = new JobDescription(sd);
         ResourceBroker broker = GAT.createResourceBroker(context, prefs);
         Job job = broker.submitJob(jd);
+
+        while ((job.getState() != Job.STOPPED)
+                && (job.getState() != Job.SUBMISSION_ERROR)) {
+            Thread.sleep(1000);
+        }
+
+        GAT.end();
     }
 }
