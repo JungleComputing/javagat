c8dcc583883278e84be7413d5fd9a8e247eb3a3e
Roelof Kemp <r.kemp@cs.vu.nl>
2008-01-17 08:26:01 +0000

    roelof: - added javadoc to JavaSoftwareDescription

--- api/src/org/gridlab/gat/resources/JavaSoftwareDescription.java
+++ api/src/org/gridlab/gat/resources/JavaSoftwareDescription.java
@@ -22,7 +22,8 @@ import java.util.Map;
  * <ul>
  * <li> java.main (String): the fully quantified name of your class
  * <li> java.options (String[]): the jvm options
- * <li> java.system.properties (String[]): the java system properties {"a=b", "c=d"}
+ * <li> java.system.properties (String[]): the java system properties {"a=b",
+ * "c=d"}
  * <li> java.arguments(String[]): The arguments for your main class
  * </ul>
  */
@@ -54,46 +55,109 @@ public class JavaSoftwareDescription extends SoftwareDescription {
     public JavaSoftwareDescription(Map<String, Object> attributes) {
         super(attributes);
         main = (String) attributes.get("java.main");
-        options = (String[]) attributes.get("java.options");
+        setOptions((String[]) attributes.get("java.options"));
         systemProperties = (String[]) attributes.get("java.system.properties");
         javaArguments = (String[]) attributes.get("java.arguments");
     }
 
+    /**
+     * @return Returns the jvm options.
+     */
     public String[] getOptions() {
         return options;
     }
 
+    /**
+     * Set the jvm options. Note that any option starting with -D or "-D will be
+     * ignored. These should be set using the setSystemProperties
+     * 
+     * @param options
+     *                the jvm options.
+     */
     public void setOptions(String[] options) {
-        this.options = options;
+        int systemProperties = 0;
+        for (String option : options) {
+            if (option.startsWith("-D") || option.startsWith("\"-D")) {
+                systemProperties++;
+            }
+        }
+        int pos = 0;
+        this.options = new String[options.length - systemProperties];
+        for (String option : options) {
+            this.options[pos++] = option;
+        }
     }
 
+    /**
+     * @return Returns the java system properties.
+     */
     public String[] getSystemProperties() {
         return systemProperties;
     }
 
+    /**
+     * Set the system properties. A system property should be passed as a=b, not
+     * as -Da=b, JavaGAT will add the -D to the property.
+     * 
+     * @param systemProperties
+     *                the system properties.
+     */
     public void setSystemProperties(String[] systemProperties) {
         this.systemProperties = systemProperties;
     }
 
+    /**
+     * @return Returns the main class.
+     */
     public String getMain() {
         return main;
     }
 
+    /**
+     * Set the main class.
+     * 
+     * @param main
+     *                the main class.
+     */
     public void setMain(String main) {
         this.main = main;
     }
 
+    /**
+     * @return Returns the arguments for the main class
+     */
     public String[] getJavaArguments() {
         return javaArguments;
     }
 
+    /**
+     * Set the arguments of the java main class
+     * 
+     * @param javaArguments
+     *                the arguments of the java main class.
+     */
     public void setJavaArguments(String[] javaArguments) {
         this.javaArguments = javaArguments;
     }
 
+    /**
+     * This method should not be used, and will ignore all arguments. The
+     * methods setOptions, setSystemProperties, setMain and setJavaArguments
+     * should be used to construct the command line arguments.
+     * 
+     * @param arguments
+     *                the command line arguments
+     */
     public void setArguments(String[] arguments) {
     }
 
+    /**
+     * This method constructs the command line arguments from the jvm options,
+     * the system properties, the main and the java arguments of this
+     * SoftwareDescription.
+     * 
+     * @returns the command line arguments
+     */
     public String[] getArguments() {
         String result[] = new String[options.length + systemProperties.length
                 + 1 + javaArguments.length];
@@ -111,6 +175,12 @@ public class JavaSoftwareDescription extends SoftwareDescription {
         return result;
     }
 
+    /**
+     * Returns the executable. If no executable is set the default executable
+     * will be "java"
+     * 
+     * @return Returns the executable.
+     */
     public String getExecutable() {
         if (super.getExecutable() == null) {
             return "java";
