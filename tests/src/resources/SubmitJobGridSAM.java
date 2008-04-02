fd255f48478ad1e926c5e6ddd840f5a6462664a0
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2008-04-02 20:10:00 +0000

    Updated gridsam test

--- tests/src/resources/SubmitJobGridSAM.java
+++ tests/src/resources/SubmitJobGridSAM.java
@@ -33,11 +33,10 @@ public class SubmitJobGridSAM {
         GATContext context = new GATContext();
         
         Preferences prefs = new Preferences();
-        // System.getProperties().setProperty("user.name", "mwi300");
         
         prefs.put("ResourceBroker.adaptor.name", "GridSAM");
+        prefs.put("ResourceBroker.sandbox.root", "/tmp");
 
-        prefs.put("ResourceBroker.sandbox.host", "localhost:4567");
 //        prefs.put("File.adaptor.name", "commandlinessh");
 //        prefs.put("File.adaptor.name", "ssh");
         SoftwareDescription sd = new SoftwareDescription();
@@ -48,7 +47,6 @@ public class SubmitJobGridSAM {
         Map<String, Object> attributes = new HashMap<String, Object>();
         attributes.put("maxCPUTime", "60");
         attributes.put("maxMemory", "90");
-        attributes.put("sandbox.root", "/tmp");
         
         Map<String, Object> env = new HashMap<String, Object>();
         env.put("ENV1", "env1val");
@@ -59,11 +57,11 @@ public class SubmitJobGridSAM {
         sd.setStdout(GAT.createFile(context, prefs, new URI("printenv.out")));
 
         ResourceDescription rd = new HardwareResourceDescription();
-        rd.addResourceAttribute("machine.node", "fs0.das3.cs.vu.nl");
+        rd.addResourceAttribute("machine.node", "titan.cs.vu.nl");
 
         JobDescription jd = new JobDescription(sd, rd);
         ResourceBroker broker = GAT.createResourceBroker(context, prefs,
-                new URI("https://localhost:18443/gridsam/services/gridsam"));
+                new URI("https://titan.cs.vu.nl:18443/gridsam/services/gridsam"));
         Job job = broker.submitJob(jd, new GridSAMMetricListener(), "job.status");
         
         while (true) {
@@ -85,6 +83,10 @@ public class SubmitJobGridSAM {
     }
     
     public static void main(String[] args) throws Exception {
-        new SubmitJobGridSAM().start(args);
+        try {
+            new SubmitJobGridSAM().start(args);
+        } catch(Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
