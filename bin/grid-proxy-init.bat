c2b04ce40ced98531e45f4cf10d34c28d1d5d279
Roelof Kemp <r.kemp@cs.vu.nl>
2008-04-29 08:31:36 +0000

    roelof: - changed windows versions of grid-proxy-*

--- bin/grid-proxy-init.bat
+++ bin/grid-proxy-init.bat
@@ -1,3 +1,2 @@
 @echo off
-
-java -DUID=%USERNAME% -classpath %GAT_LOCATION%\lib\adaptors\GlobusAdaptor\cog-jglobus.jar org.globus.tools.ProxyInit
+java -DUID="%USERNAME%" -classpath "%GAT_LOCATION%\lib\adaptors\GlobusAdaptor\cog-jglobus.jar" org.globus.tools.ProxyInit
