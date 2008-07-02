1e0559efc35e105493d42be9c3d2ae9da6b67770
Roelof Kemp <r.kemp@cs.vu.nl>
2008-07-02 12:07:44 +0000

    roelof: - improved finding of closest file for logical file

--- adaptors/Local/src/org/gridlab/gat/io/cpi/local/LocalLogicalFileAdaptor.java
+++ adaptors/Local/src/org/gridlab/gat/io/cpi/local/LocalLogicalFileAdaptor.java
@@ -3,6 +3,7 @@
 package org.gridlab.gat.io.cpi.local;
 
 import org.gridlab.gat.GATContext;
+import org.gridlab.gat.URI;
 import org.gridlab.gat.io.cpi.LogicalFileCpi;
 
 @SuppressWarnings("serial")
@@ -22,5 +23,34 @@ public class LocalLogicalFileAdaptor extends LogicalFileCpi {
     public LocalLogicalFileAdaptor(GATContext gatContext, String name,
             Integer mode) throws Exception {
         super(gatContext, name, mode);
+    }
+    
+    protected URI getClosestFile(URI loc) {
+        // first check: same hostname
+        for (URI file : files) {
+            if (file.getHost().equalsIgnoreCase(loc.getHost())) {
+                return file;
+            }
+        }
+        // check for same suffix. The more parts of the suffix are the same, the closer the location
+        String locationPart = loc.getHost();
+        while (locationPart.contains(".")) {
+            int position = locationPart.indexOf(".");
+            for (URI file : files) {
+                if (file.getHost().endsWith(locationPart.substring(position))) {
+                    return file;
+                }
+            }
+            locationPart = locationPart.substring(position);
+        }
+        int separatorPosition = loc.getHost().indexOf(".");
+        if (separatorPosition > 0) {
+            for (URI file : files) {
+                if (file.getHost().endsWith(loc.getHost().substring(separatorPosition))) {
+                    return file;
+                }
+            }
+        }
+        return files.get(0);
     }
 }
