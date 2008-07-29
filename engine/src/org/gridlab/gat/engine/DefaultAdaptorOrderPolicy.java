c03f14e5e4dc813f05f9d50eebc7ad69129f1f17
Roelof Kemp <r.kemp@cs.vu.nl>
2008-07-29 12:06:18 +0000

    roelof:

--- engine/src/org/gridlab/gat/engine/DefaultAdaptorOrderPolicy.java
+++ engine/src/org/gridlab/gat/engine/DefaultAdaptorOrderPolicy.java
@@ -43,52 +43,53 @@ public class DefaultAdaptorOrderPolicy implements AdaptorOrderPolicy {
 
     private void orderFileList(List<Adaptor> l) {
         int pos = 0;
-        placeAdaptor(pos++, "local", "file", l);
-        placeAdaptor(pos++, "gridftp", "file", l);
-        placeAdaptor(pos++, "sftpganymed", "file", l);
-        placeAdaptor(pos++, "commandlinessh", "file", l);
-        placeAdaptor(pos++, "sftpnew", "file", l);
-        placeAdaptor(pos++, "sshtrilead", "file", l);
-        placeAdaptor(pos++, "sftp", "file", l);
+        pos = placeAdaptor(pos, "local", "file", l);
+        pos = placeAdaptor(pos, "gridftp", "file", l);
+        pos = placeAdaptor(pos, "sftpganymed", "file", l);
+        pos = placeAdaptor(pos, "commandlinessh", "file", l);
+        pos = placeAdaptor(pos, "sftpnew", "file", l);
+        pos = placeAdaptor(pos, "sshtrilead", "file", l);
+        pos = placeAdaptor(pos, "sftp", "file", l);
         // rest in random order
     }
 
     private void orderFileInputStreamList(List<Adaptor> l) {
         int pos = 0;
-        placeAdaptor(pos++, "local", "fileinputstream", l);
-        placeAdaptor(pos++, "gridftp", "fileinputstream", l);
-        placeAdaptor(pos++, "sftpnew", "fileinputstream", l);
-        placeAdaptor(pos++, "sftp", "fileinputstream", l);
-        placeAdaptor(pos++, "copying", "fileinputstream", l);
+        pos = placeAdaptor(pos, "local", "fileinputstream", l);
+        pos = placeAdaptor(pos, "gridftp", "fileinputstream", l);
+        pos = placeAdaptor(pos, "sftpnew", "fileinputstream", l);
+        pos = placeAdaptor(pos, "sftp", "fileinputstream", l);
+        pos = placeAdaptor(pos, "copying", "fileinputstream", l);
         // rest in random order
     }
 
     private void orderFileOutputStreamList(List<Adaptor> l) {
         int pos = 0;
-        placeAdaptor(pos++, "local", "fileoutputstream", l);
-        placeAdaptor(pos++, "gridftp", "fileoutputstream", l);
-        placeAdaptor(pos++, "sftp", "fileoutputstream", l);
+        pos = placeAdaptor(pos, "local", "fileoutputstream", l);
+        pos = placeAdaptor(pos, "gridftp", "fileoutputstream", l);
+        pos = placeAdaptor(pos, "sftp", "fileoutputstream", l);
         // rest in random order
     }
 
     private void orderResourceList(List<Adaptor> l) {
         int pos = 0;
-        placeAdaptor(pos++, "local", "resourcebroker", l);
-        placeAdaptor(pos++, "globus", "resourcebroker", l);
-        placeAdaptor(pos++, "wsgt4new", "resourcebroker", l);
-        placeAdaptor(pos++, "sshtrilead", "resourcebroker", l);
+        pos = placeAdaptor(pos, "local", "resourcebroker", l);
+        pos = placeAdaptor(pos, "globus", "resourcebroker", l);
+        pos = placeAdaptor(pos, "wsgt4new", "resourcebroker", l);
+        pos = placeAdaptor(pos, "sshtrilead", "resourcebroker", l);
         // rest in random order
     }
-    
-    private void placeAdaptor(int position, String adaptorName, String cpiName, List<Adaptor> l) {
+
+    private int placeAdaptor(int position, String adaptorName, String cpiName,
+            List<Adaptor> l) {
         int currentPosition = l.indexOf(getAdaptor(adaptorName, cpiName, l));
         if (currentPosition < 0) {
-        	return;
+            return position;
         }
         l.add(position, l.remove(currentPosition));
-        return;
+        return position++;
     }
-    
+
     private Adaptor getAdaptor(String shortName, String cpiName,
             List<Adaptor> adaptors) {
         for (Adaptor adaptor : adaptors) {
