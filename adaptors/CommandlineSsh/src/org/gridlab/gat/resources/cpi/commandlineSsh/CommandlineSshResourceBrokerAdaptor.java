2d957f400b9f3c18fd35d5edd71d2e3bb79dfe3d
Jason Maassen <j.maassen@esciencecenter.nl>
2008-07-30 15:08:56 +0000

    roelof: - fix in sandbox for commandline ssh resourcebroker

--- adaptors/CommandlineSsh/src/org/gridlab/gat/resources/cpi/commandlineSsh/CommandlineSshResourceBrokerAdaptor.java
+++ adaptors/CommandlineSsh/src/org/gridlab/gat/resources/cpi/commandlineSsh/CommandlineSshResourceBrokerAdaptor.java
@@ -27,228 +27,228 @@ import org.gridlab.gat.security.commandlinessh.CommandlineSshSecurityUtils;
 
 public class CommandlineSshResourceBrokerAdaptor extends ResourceBrokerCpi {
 
-    public static Map<String, Boolean> getSupportedCapabilities() {
-        Map<String, Boolean> capabilities = ResourceBrokerCpi
-                .getSupportedCapabilities();
-        capabilities.put("submitJob", true);
+	public static Map<String, Boolean> getSupportedCapabilities() {
+		Map<String, Boolean> capabilities = ResourceBrokerCpi
+				.getSupportedCapabilities();
+		capabilities.put("submitJob", true);
 
-        return capabilities;
-    }
+		return capabilities;
+	}
 
-    protected static Logger logger = Logger
-            .getLogger(CommandlineSshResourceBrokerAdaptor.class);
+	protected static Logger logger = Logger
+			.getLogger(CommandlineSshResourceBrokerAdaptor.class);
 
-    public static final int SSH_PORT = 22;
+	public static final int SSH_PORT = 22;
 
-    private boolean windows = false;
+	private boolean windows = false;
 
-    /**
-     * This method constructs a CommandlineSshResourceBrokerAdaptor instance
-     * corresponding to the passed GATContext.
-     * 
-     * @param gatContext
-     *                A GATContext which will be used to broker resources
-     */
-    public CommandlineSshResourceBrokerAdaptor(GATContext gatContext,
-            URI brokerURI) throws GATObjectCreationException {
-        super(gatContext, brokerURI);
+	/**
+	 * This method constructs a CommandlineSshResourceBrokerAdaptor instance
+	 * corresponding to the passed GATContext.
+	 * 
+	 * @param gatContext
+	 *            A GATContext which will be used to broker resources
+	 */
+	public CommandlineSshResourceBrokerAdaptor(GATContext gatContext,
+			URI brokerURI) throws GATObjectCreationException {
+		super(gatContext, brokerURI);
 
-        if (!brokerURI.isCompatible("ssh") && brokerURI.getScheme() != null
-                || (brokerURI.refersToLocalHost() && (brokerURI == null))) {
-            throw new AdaptorNotApplicableException(
-                    "cannot handle the scheme, scheme is: "
-                            + brokerURI.getScheme());
-        }
+		if (!brokerURI.isCompatible("ssh") && brokerURI.getScheme() != null
+				|| (brokerURI.refersToLocalHost() && (brokerURI == null))) {
+			throw new AdaptorNotApplicableException(
+					"cannot handle the scheme, scheme is: "
+							+ brokerURI.getScheme());
+		}
 
-        String osname = System.getProperty("os.name");
-        if (osname.startsWith("Windows")) {
-            windows = true;
-        }
-    }
+		String osname = System.getProperty("os.name");
+		if (osname.startsWith("Windows")) {
+			windows = true;
+		}
+	}
 
-    public Job submitJob(AbstractJobDescription abstractDescription,
-            MetricListener listener, String metricDefinitionName)
-            throws GATInvocationException {
+	public Job submitJob(AbstractJobDescription abstractDescription,
+			MetricListener listener, String metricDefinitionName)
+			throws GATInvocationException {
 
-        if (!(abstractDescription instanceof JobDescription)) {
-            throw new GATInvocationException(
-                    "can only handle JobDescriptions: "
-                            + abstractDescription.getClass());
-        }
+		if (!(abstractDescription instanceof JobDescription)) {
+			throw new GATInvocationException(
+					"can only handle JobDescriptions: "
+							+ abstractDescription.getClass());
+		}
 
-        JobDescription description = (JobDescription) abstractDescription;
+		JobDescription description = (JobDescription) abstractDescription;
 
-        SoftwareDescription sd = description.getSoftwareDescription();
-        if (sd == null) {
-            throw new GATInvocationException(
-                    "The job description does not contain a software description");
-        }
-        if (description.getProcessCount() != 1) {
-            throw new GATInvocationException(
-                    "Adaptor cannot handle: process count > 1: "
-                            + description.getProcessCount());
-        }
+		SoftwareDescription sd = description.getSoftwareDescription();
+		if (sd == null) {
+			throw new GATInvocationException(
+					"The job description does not contain a software description");
+		}
+		if (description.getProcessCount() != 1) {
+			throw new GATInvocationException(
+					"Adaptor cannot handle: process count > 1: "
+							+ description.getProcessCount());
+		}
 
-        if (description.getResourceCount() != 1) {
-            throw new GATInvocationException(
-                    "Adaptor cannot handle: resource count > 1: "
-                            + description.getResourceCount());
-        }
+		if (description.getResourceCount() != 1) {
+			throw new GATInvocationException(
+					"Adaptor cannot handle: resource count > 1: "
+							+ description.getResourceCount());
+		}
 
-        // we do not support environment yet
-        Map<String, Object> env = sd.getEnvironment();
-        if (env != null && !env.isEmpty()) {
-            throw new MethodNotApplicableException("cannot handle environment");
-        }
+		// we do not support environment yet
+		Map<String, Object> env = sd.getEnvironment();
+		if (env != null && !env.isEmpty()) {
+			throw new MethodNotApplicableException("cannot handle environment");
+		}
 
-        String path = getExecutable(description);
-        String authority = getAuthority();
-        if (authority == null) {
-            authority = "localhost";
-        }
+		String path = getExecutable(description);
+		String authority = getAuthority();
+		if (authority == null) {
+			authority = "localhost";
+		}
 
-        String host = getHostname();
-        if (host == null) {
-            host = "localhost";
-        }
+		String host = getHostname();
+		if (host == null) {
+			host = "localhost";
+		}
 
-        /* allow port override */
-        int port = brokerURI.getPort();
-        /* it will always return -1 for user@host:path */
-        if (port == -1) {
-            port = SSH_PORT;
-        }
+		/* allow port override */
+		int port = brokerURI.getPort();
+		/* it will always return -1 for user@host:path */
+		if (port == -1) {
+			port = SSH_PORT;
+		}
 
-        // create the sandbox
-        Sandbox sandbox = new Sandbox(gatContext, description, authority, null,
-                true, false, false, false);
-        // create the job
-        CommandlineSshJob commandlineSshJob = new CommandlineSshJob(gatContext,
-                description, sandbox);
-        Job job = null;
-        if (description instanceof WrapperJobDescription) {
-            WrapperJobCpi tmp = new WrapperJobCpi(commandlineSshJob);
-            listener = tmp;
-            job = tmp;
-        } else {
-            job = commandlineSshJob;
-        }
-        if (listener != null && metricDefinitionName != null) {
-            Metric metric = job.getMetricDefinitionByName(metricDefinitionName)
-                    .createMetric(null);
-            job.addMetricListener(listener, metric);
-        }
-        // set the state to prestaging
-        commandlineSshJob.setState(Job.JobState.PRE_STAGING);
-        // and let the sandbox prestage the files!
-        sandbox.prestage();
+		// create the sandbox
+		Sandbox sandbox = new Sandbox(gatContext, description, authority, null,
+				true, false, false, false);
+		// create the job
+		CommandlineSshJob commandlineSshJob = new CommandlineSshJob(gatContext,
+				description, sandbox);
+		Job job = null;
+		if (description instanceof WrapperJobDescription) {
+			WrapperJobCpi tmp = new WrapperJobCpi(commandlineSshJob);
+			listener = tmp;
+			job = tmp;
+		} else {
+			job = commandlineSshJob;
+		}
+		if (listener != null && metricDefinitionName != null) {
+			Metric metric = job.getMetricDefinitionByName(metricDefinitionName)
+					.createMetric(null);
+			job.addMetricListener(listener, metric);
+		}
+		// set the state to prestaging
+		commandlineSshJob.setState(Job.JobState.PRE_STAGING);
+		// and let the sandbox prestage the files!
+		sandbox.prestage();
 
-        Map<String, String> securityInfo = CommandlineSshSecurityUtils
-                .getSshCredential(gatContext, "commandlinessh", brokerURI,
-                        SSH_PORT);
-        String username = securityInfo.get("username");
-        String password = securityInfo.get("password");
-        int privateKeySlot = -1;
-        try {
-            privateKeySlot = Integer.parseInt(securityInfo
-                    .get("privatekeyslot"));
-        } catch (NumberFormatException e) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("unable to parse private key slot: " + e);
-            }
-        }
+		Map<String, String> securityInfo = CommandlineSshSecurityUtils
+				.getSshCredential(gatContext, "commandlinessh", brokerURI,
+						SSH_PORT);
+		String username = securityInfo.get("username");
+		String password = securityInfo.get("password");
+		int privateKeySlot = -1;
+		try {
+			privateKeySlot = Integer.parseInt(securityInfo
+					.get("privatekeyslot"));
+		} catch (NumberFormatException e) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("unable to parse private key slot: " + e);
+			}
+		}
 
-        String command = null;
-        if (windows) {
-            command = "sexec " + username + "@" + authority
-                    + " -unat=yes -cmd=" + path + " "
-                    + getArguments(description);
-            if (password == null) { // public/private key
-                int slot = privateKeySlot;
-                if (slot == -1) { // not set by the user, assume he only has
-                    // one key
-                    slot = 0;
-                }
-                command += " -pk=" + slot;
-            } else { // password
-                command += " -pw=" + password;
-            }
-        } else {
-            // we must use the -t option to ssh (allocates pseudo TTY).
-            // If we don't, there is no way to kill the remote process.
-            command = "ssh -p " + port + " "
-                    + "-o BatchMode=yes -o StrictHostKeyChecking=yes -t -t "
-                    + username + "@" + host + " ";
-            if (sandbox.getSandboxPath() != null) {
-                command += "cd " + sandbox.getSandbox() + " && ";
-            }
-            command += path + " " + getArguments(description);
-        }
+		String command = null;
+		if (windows) {
+			command = "sexec " + username + "@" + authority
+					+ " -unat=yes -cmd=" + path + " "
+					+ getArguments(description);
+			if (password == null) { // public/private key
+				int slot = privateKeySlot;
+				if (slot == -1) { // not set by the user, assume he only has
+					// one key
+					slot = 0;
+				}
+				command += " -pk=" + slot;
+			} else { // password
+				command += " -pw=" + password;
+			}
+		} else {
+			// we must use the -t option to ssh (allocates pseudo TTY).
+			// If we don't, there is no way to kill the remote process.
+			command = "ssh -p " + port + " "
+					+ "-o BatchMode=yes -o StrictHostKeyChecking=yes -t -t "
+					+ username + "@" + host + " ";
+			if (sandbox.getSandboxPath() != null) {
+				command += "cd " + sandbox.getSandboxPath() + " && ";
+			}
+			command += path + " " + getArguments(description);
+		}
 
-        if (logger.isInfoEnabled()) {
-            logger.info("running command: " + command);
-        }
+		if (logger.isInfoEnabled()) {
+			logger.info("running command: " + command);
+		}
 
-        Process p = null;
-        try {
-            p = Runtime.getRuntime().exec(command.toString());
-        } catch (IOException e) {
-            throw new CommandNotFoundException(
-                    "CommandlineSshResourceBrokerAdaptor", e);
-        }
-        commandlineSshJob.setState(Job.JobState.RUNNING);
-        commandlineSshJob.setSubmissionTime();
-        commandlineSshJob.setStartTime();
-        commandlineSshJob.setProcess(p);
+		Process p = null;
+		try {
+			p = Runtime.getRuntime().exec(command.toString());
+		} catch (IOException e) {
+			throw new CommandNotFoundException(
+					"CommandlineSshResourceBrokerAdaptor", e);
+		}
+		commandlineSshJob.setState(Job.JobState.RUNNING);
+		commandlineSshJob.setSubmissionTime();
+		commandlineSshJob.setStartTime();
+		commandlineSshJob.setProcess(p);
 
-        if (!sd.streamingStderrEnabled()) {
-            // read away the stderr
+		if (!sd.streamingStderrEnabled()) {
+			// read away the stderr
 
-            try {
-                if (sd.getStderr() != null) {
-                    // to file
-                    new StreamForwarder(p.getErrorStream(), GAT
-                            .createFileOutputStream(sd.getStderr()));
-                } else {
-                    // or throw it away
-                    new StreamForwarder(p.getErrorStream(), null);
-                }
-            } catch (GATObjectCreationException e) {
-                throw new GATInvocationException(
-                        "Unable to create file output stream for stderr!", e);
-            }
-        }
+			try {
+				if (sd.getStderr() != null) {
+					// to file
+					new StreamForwarder(p.getErrorStream(), GAT
+							.createFileOutputStream(sd.getStderr()));
+				} else {
+					// or throw it away
+					new StreamForwarder(p.getErrorStream(), null);
+				}
+			} catch (GATObjectCreationException e) {
+				throw new GATInvocationException(
+						"Unable to create file output stream for stderr!", e);
+			}
+		}
 
-        if (!sd.streamingStdoutEnabled()) {
-            // read away the stdout
-            try {
-                if (sd.getStdout() != null) {
-                    // to file
-                    new StreamForwarder(p.getInputStream(), GAT
-                            .createFileOutputStream(sd.getStdout()));
-                } else {
-                    // or throw it away
-                    new StreamForwarder(p.getInputStream(), null);
-                }
-            } catch (GATObjectCreationException e) {
-                throw new GATInvocationException(
-                        "Unable to create file output stream for stdout!", e);
-            }
-        }
+		if (!sd.streamingStdoutEnabled()) {
+			// read away the stdout
+			try {
+				if (sd.getStdout() != null) {
+					// to file
+					new StreamForwarder(p.getInputStream(), GAT
+							.createFileOutputStream(sd.getStdout()));
+				} else {
+					// or throw it away
+					new StreamForwarder(p.getInputStream(), null);
+				}
+			} catch (GATObjectCreationException e) {
+				throw new GATInvocationException(
+						"Unable to create file output stream for stdout!", e);
+			}
+		}
 
-        if (!sd.streamingStdinEnabled() && sd.getStdin() != null) {
-            // forward the stdin from file
-            try {
-                new StreamForwarder(GAT.createFileInputStream(sd.getStdin()), p
-                        .getOutputStream());
-            } catch (GATObjectCreationException e) {
-                throw new GATInvocationException(
-                        "Unable to create file input stream for stdin!", e);
-            }
-        }
+		if (!sd.streamingStdinEnabled() && sd.getStdin() != null) {
+			// forward the stdin from file
+			try {
+				new StreamForwarder(GAT.createFileInputStream(sd.getStdin()), p
+						.getOutputStream());
+			} catch (GATObjectCreationException e) {
+				throw new GATInvocationException(
+						"Unable to create file input stream for stdin!", e);
+			}
+		}
 
-        commandlineSshJob.monitorState();
+		commandlineSshJob.monitorState();
 
-        return job;
-    }
+		return job;
+	}
 }
