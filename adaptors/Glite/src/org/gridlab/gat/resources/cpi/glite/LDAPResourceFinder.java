3b8c7e0a2ed27ce67a7bcfecd40e24f90007ac46
Roelof Kemp <r.kemp@cs.vu.nl>
2008-09-09 07:28:21 +0000

    roelof: - removed unnecessary imports from LDAPResourceFinder (glite)

--- adaptors/Glite/src/org/gridlab/gat/resources/cpi/glite/LDAPResourceFinder.java
+++ adaptors/Glite/src/org/gridlab/gat/resources/cpi/glite/LDAPResourceFinder.java
@@ -3,7 +3,6 @@ package org.gridlab.gat.resources.cpi.glite;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
 
 import javax.naming.Context;
 import javax.naming.NamingEnumeration;
@@ -16,8 +15,6 @@ import javax.naming.ldap.InitialLdapContext;
 
 import org.apache.log4j.Logger;
 import org.gridlab.gat.URI;
-import org.gridlab.gat.resources.HardwareResourceDescription;
-import org.gridlab.gat.resources.SoftwareResourceDescription;
 
 public class LDAPResourceFinder {
 	
