819b39f495ac52ecf08466cf1f608967471a445b
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2008-12-01 12:36:52 +0000

    Removed unused import

--- api/src/org/gridlab/gat/resources/JavaSoftwareDescription.java
+++ api/src/org/gridlab/gat/resources/JavaSoftwareDescription.java
@@ -7,7 +7,6 @@ package org.gridlab.gat.resources;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * An instance of this class is a description of a piece of java software
