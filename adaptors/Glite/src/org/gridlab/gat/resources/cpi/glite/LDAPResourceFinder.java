6e2efc899f7293ffed920d492f0b8003c9717973
Max Berger <maximilian.berger@uibk.ac.at>
2009-01-07 15:06:31 +0000

    added support for SEs and LFCs code cleanups

--- adaptors/Glite/src/org/gridlab/gat/resources/cpi/glite/LDAPResourceFinder.java
+++ adaptors/Glite/src/org/gridlab/gat/resources/cpi/glite/LDAPResourceFinder.java
@@ -6,6 +6,10 @@
 // Jun,Jul/2008 - Thomas Zangerl 
 //      for Distributed and Parallel Systems Research Group
 //      University of Innsbruck
+// Jan/2009 - Max Berger 
+//      for Distributed and Parallel Systems Research Group
+//      University of Innsbruck
+//      SE/LFC implementations.
 //
 ////////////////////////////////////////////////////////////////////
 
@@ -31,24 +35,38 @@ public class LDAPResourceFinder {
 	
     protected static final Logger logger = Logger.getLogger(LDAPResourceFinder.class);
 	
-	public static final String LDAP_SERVER_NAME = "ldap://bdii.ce-egee.org";
-	public static final int LDAP_SERVER_PORT = 2170;
+	public static final String DEFAULT_LDAP_SERVER_NAME = "ldap://bdii.ce-egee.org";
+	public static final int DEFAULT_LDAP_SERVER_PORT = 2170;
 	
 	private static final String START_DN = "Mds-Vo-name=local,o=Grid";
 	
-	private String ldapContact;
-	private DirContext ctx;
-	private Hashtable < String, String > env;
-	
-	private SearchControls globalSearchControls;
+	private final DirContext ctx;
+
+	private final SearchControls globalSearchControls;
 	
 	
+	/**
+     * Create a new LDAPResourceFinder with the given BDII.
+     * 
+     * @param ldapResource
+     *            URI to a BDII, if null a default value will be used.
+     * @throws NamingException
+     *             if an LDAP error occurs.
+     */
 	public LDAPResourceFinder(URI ldapResource) throws NamingException {
-		if (ldapResource == null) {
-			this.ldapContact = LDAP_SERVER_NAME + ":" + LDAP_SERVER_PORT;
-		} else {
-			this.ldapContact = ldapResource.toString();
-		}
+        final String ldapContact;
+        if (ldapResource == null) {
+            ldapContact = DEFAULT_LDAP_SERVER_NAME + ":"
+                    + DEFAULT_LDAP_SERVER_PORT;
+        } else {
+            final String host = ldapResource.getHost();
+            final int port = ldapResource.getPort(DEFAULT_LDAP_SERVER_PORT);
+            if (host == null) {
+                ldapContact = DEFAULT_LDAP_SERVER_NAME + ":" + port;
+            } else {
+                ldapContact = host + ":" + port;
+            }
+        }
 		
 		globalSearchControls = new SearchControls();
 		// search everything within the given context
@@ -60,14 +78,33 @@ public class LDAPResourceFinder {
 		// return all attributes
 		globalSearchControls.setReturningAttributes(null);
 		
-		env = new Hashtable<String, String>();
+		final Hashtable<String, String> env = new Hashtable<String, String>();
 		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
 		env.put(Context.PROVIDER_URL, ldapContact);
 		
-		// construct the ldap connection from the environement params
+		// construct the ldap connection from the environment params
 		ctx = new InitialLdapContext(env, null);
 	}
+
+    private String getSafeStringAttr(SearchResult result, String name)
+            throws NamingException {
+        Attribute attr = result.getAttributes().get(name);
+        if (attr != null) {
+            return (String) attr.get();
+        } else
+            return null;
+    }
+
 	
+	/**
+     * Retrieve a list of WMSs for the given VO.
+     * 
+     * @param voName
+     *            name of the VO. Must not be null.
+     * @return a List&lt;String&gt; of WMS URIs
+     * @throws NamingException
+     *             if an LDAP error occurs.
+     */
 	public List<String> fetchWMSServers(final String voName) throws NamingException {
 		ArrayList<String> wmsServers = new ArrayList<String>();
 		
@@ -76,15 +113,23 @@ public class LDAPResourceFinder {
 		
 		while (results.hasMore()) {
 			SearchResult result = (SearchResult) results.nextElement();
-			Attribute wmsServerAtt = result.getAttributes().get("GlueServiceAccessPointURL");
-			String wmsServer = (String) wmsServerAtt.get();
+			String wmsServer = getSafeStringAttr(result, "GlueServiceAccessPointURL");
 			logger.info("Retrieved the following WMS server from LDAP: " + wmsServer);
-			wmsServers.add(wmsServer);
+			if (wmsServer!=null) wmsServers.add(wmsServer);
 		}
 		
 		return wmsServers;
 	}
 	
+    /**
+     * Retrieve a list of CEs for the given VO.
+     * 
+     * @param voName
+     *            name of the VO. Must not be null.
+     * @return a List&lt;String&gt; of CE Ids
+     * @throws NamingException
+     *             if an LDAP error occurs.
+     */
 	public List<String> fetchCEs(final String voName) 
 	throws NamingException {
 		ArrayList<String> results = new ArrayList<String>();	
@@ -94,15 +139,126 @@ public class LDAPResourceFinder {
 		
 		while (clusters.hasMore()) {
 			SearchResult result = (SearchResult) clusters.nextElement();
-			Attribute ceAtt = result.getAttributes().get("GlueCEInfoContactString");
-			String ceURL = (String) ceAtt.get();
+			String ceURL = getSafeStringAttr(result, "GlueCEInfoContactString");
 			logger.info("Retrieved the following CE from LDAP: " + ceURL);
-			results.add(ceURL);
+			if (ceURL!=null) results.add(ceURL);
 		}
 		
 		return results;
 	}
 	
+    /**
+     * Retrieve a list of LFCs for the given VO.
+     * 
+     * @param voName
+     *            name of the VO. Must not be null.
+     * @return a List&lt;String&gt; of LFC endpoints. Under normal circumstances
+     *         this list should contain exactly one entry.
+     * @throws NamingException
+     *             if an LDAP error occurs.
+     */
+    public List<String> fetchLFCs(final String voName) throws NamingException {
+        ArrayList<String> results = new ArrayList<String>();
+
+        String filter = "(&(objectClass~=GlueService)(GlueServiceType=lcg-file-catalog)(GlueServiceOwner="
+                + voName + "))";
+        NamingEnumeration<SearchResult> searchResults = ctx.search(START_DN, filter,
+                globalSearchControls);
+        while (searchResults.hasMore()) {
+            SearchResult result = (SearchResult) searchResults.nextElement();
+            String endpoint = getSafeStringAttr(result, "GlueServiceEndpoint");
+            results.add(endpoint);
+            logger.info("Retrieved the following LFC from LDAP: " + endpoint);
+        }
+        return results;
+    }
+
+    /**
+     * Contains information about a storage elmenet (SE).
+     */
+    public class SEInfo {
+        final String seUniqueId;
+        final String path;
+        final String space;
+        private SEInfo(String id, String pat, String spac) {
+            this.seUniqueId = id;
+            this.path = pat;
+            this.space = spac;
+        }
+        /**
+         * @return the uniqieID of the SE.
+         */
+        public String getSeUniqueId() {
+            return seUniqueId;
+        }
+        /**
+         * @return the storage path on the SE.
+         */
+        public String getPath() {
+            return path;
+        }
+        /**
+         * @return the amount of free space on the SE.
+         */
+        public String getSpace() {
+            return space;
+        }
+        @Override
+        public String toString() {
+            return "{"+seUniqueId+","+path+","+space+"}";
+        }        
+    }
+    
+    /**
+     * Retrieve a list of SEs for the given VO.
+     * 
+     * @param voName
+     *            name of the VO. Must not be null.
+     * @return a List&lt;{@link SEInfo}&gt; of SEs
+     * @throws NamingException
+     *             if an LDAP error occurs.
+     */
+    public List<SEInfo> fetchSEs(final String voName) throws NamingException {
+        final ArrayList<SEInfo> results = new ArrayList<SEInfo>();
+
+        final String filter = "(&(objectClass~=GlueSA)(GlueSALocalID=" + voName
+                + "))";
+        final NamingEnumeration<SearchResult> searchResults = ctx.search(START_DN,
+                filter, globalSearchControls);
+
+        while (searchResults.hasMore()) {
+            final SearchResult result = (SearchResult) searchResults.nextElement();
+            String pathName = result.getName();
+            String seUniqueId = null;
+            int pos = pathName.indexOf("GlueSEUniqueID");
+            if (pos < 0) {
+                pathName = getSafeStringAttr(result, "GlueChunkKey");
+                if (pathName!=null)
+                pos = pathName.indexOf("GlueSEUniqueID");
+            }
+            if (pos >= 0) {
+                String st = pathName.substring(pos + 15);
+                int posEnd = st.indexOf(',');
+                if (posEnd < 0)
+                    posEnd = st.length();
+                seUniqueId = st.substring(0, posEnd);
+            }
+
+            String path = getSafeStringAttr(result,"GlueSAPath");
+            String space = getSafeStringAttr(result, 
+                    "GlueSAStateAvailableSpace");
+
+            if (seUniqueId != null) {
+                logger.info("Got SE: " + seUniqueId + " with path " + path
+                        + " and space " + space);
+                results.add(new SEInfo(seUniqueId,path,space));
+            }
+
+        }
+
+        return results;
+    }
+    
 	
 // only used for testing
 //	public static void main(final String [] args) throws Exception {
