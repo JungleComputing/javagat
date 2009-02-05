aa96ce2e77d7ec213059cf4ab38cfda2fbd41909
Bas Boterman <bbn230@few.vu.nl>
2009-02-05 14:55:35 +0000

    Updated AppEngineResourcesException class.

--- advert/ibis/advert/AppEngineResourcesException.java
+++ advert/ibis/advert/AppEngineResourcesException.java
@@ -1,5 +1,19 @@
 package ibis.advert;
 
-public class AppEngineResourcesException extends Exception {
+/**
+ *  This exception is thrown when the App Engine runs out of resources.
+ *  
+ * @author bbn230
+ *
+ */
 
+@SuppressWarnings("serial")
+public class AppEngineResourcesException extends Exception {
+    public AppEngineResourcesException() {
+        super();
+    }
+
+    public AppEngineResourcesException(String s) {
+        super(s);
+    }
 }
