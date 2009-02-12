09defa351a857d843ed9dcb96daf7760bd16f081
Bas Boterman <bbn230@few.vu.nl>
2009-02-12 17:14:19 +0000

    Finished working Java client to make HTTPS connections to the App Engine.

--- advert/ibis/advert/MetaData.java
+++ advert/ibis/advert/MetaData.java
@@ -24,7 +24,6 @@ public class MetaData {
 	/**
 	 * Constructor.
 	 */
-
 	public MetaData() {
 	}
 
--- app-engine/client/Client.java
+++ app-engine/client/Client.java
@@ -5,43 +5,105 @@
 package client;
 
 /**
- * Test class to make HTTP connections to the App Engine.
+ * Test classes to make HTTP(S) connections to the App Engine.
  * 
  * @author bbn230
  */
 
 import java.net.*;
 import java.io.*;
+import java.security.Security;
+import java.util.Properties;
 
 public class Client {
-	private static void makeHttpsConnection() throws Exception {
-		/* TODO: make HTTPS connection here. */
+	/**
+	 * This function makes an HTTPS connection to an URL defined by uri.
+	 * @param uri
+	 *     {@link String} to make the connection to.
+	 * @throws Exception
+	 *     The connection can't be established.
+	 */
+	private static void makeHttpsConnection(String uri) throws Exception {
+		String protocol = "https://";
+		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+
+		Properties properties = System.getProperties();
+
+		String handlers = System.getProperty("java.protocol.handler.pkgs");
+		if (handlers == null) {
+			/* nothing specified yet (expected case) */
+			properties.put("java.protocol.handler.pkgs",
+					"com.sun.net.ssl.internal.www.protocol");
+		} 
+		else {
+			/* something already there, put ourselves out front */
+			properties.put("java.protocol.handler.pkgs",
+					"com.sun.net.ssl.internal.www.protocol|".concat(handlers));
+		}
+		System.setProperties(properties); 
+
+		try {
+			URL page = new URL(protocol.concat(uri)); 
+			URLConnection urlc = page.openConnection();
+
+			System.out.println("*** Making an HTTPS connection to https://" + uri + " ***");
+			
+			urlc.setUseCaches(false);
+			
+			/* Retrieving headers. */
+			System.out.println("Content-type = " + urlc.getContentType());
+			System.out.println("Content-length = " + urlc.getContentLength());
+			
+			/* Retrieving body. */
+			BufferedReader in = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+			String inputLine;
+			
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println(inputLine);
+			}
+			in.close();
+		} 
+		catch (MalformedURLException mue) {
+			System.out.println("URL cannot be resolved");
+		}
 	}
-	
-	private static void makeHttpConnection() throws Exception {
-		/* Setting up a new connection. */		
-		URL url = new URL("http://daraja.appspot.com/");
-        URLConnection urlc = url.openConnection();
-        HttpURLConnection httpc = (HttpURLConnection) url.openConnection();  
-        BufferedReader in = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-        String inputLine;
-        
-        /* Retrieving headers. */
-        System.out.println(httpc.getResponseCode());
-        System.out.println(httpc.getRequestMethod());
-        for(int i=0; httpc.getHeaderField(i) != null; i++) {
-        	System.out.println(httpc.getHeaderField(i));
-        }
-       
-        /* Retrieving body. */
-        while ((inputLine = in.readLine()) != null) { 
-            System.out.println(inputLine);
-        }
-        in.close();
+
+	/**
+	 * This function makes an HTTP connection to an URL defined by uri.
+	 * @param uri
+	 *     {@link String} to make the connection to.
+	 * @throws Exception
+	 *     The connection can't be established.
+	 */
+	private static void makeHttpConnection(String uri) throws Exception {
+		/* Setting up a new connection. */
+		String protocol = "http://";
+		URL url = new URL(protocol.concat(uri));
+		URLConnection urlc = url.openConnection();
+		HttpURLConnection httpc = (HttpURLConnection) url.openConnection();
+		
+		System.out.println("*** Making an HTTP connection to http://" + uri + " ***");
+
+		/* Retrieving headers. */
+		System.out.println(httpc.getResponseCode());
+		System.out.println(httpc.getRequestMethod());
+		for (int i = 0; httpc.getHeaderField(i) != null; i++) {
+			System.out.println(httpc.getHeaderField(i));
+		}
+
+		/* Retrieving body. */
+		BufferedReader in = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+		String inputLine;
+
+		while ((inputLine = in.readLine()) != null) {
+			System.out.println(inputLine);
+		}
+		in.close();
 	}
-	
+
 	public static void main(String argv[]) throws Exception {
-		makeHttpConnection();
-		makeHttpsConnection();
+		String uri = "bbn230.appspot.com/";
+		makeHttpConnection(uri);
+		makeHttpsConnection(uri);
 	}
 }
