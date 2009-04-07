d81201568fba4b16edf8548d0aa3190dd1e84d24
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2009-04-07 11:41:11 +0000

    re-format

--- api/src/org/gridlab/gat/io/permissions/AbstractBasicFileAttributeView.java
+++ api/src/org/gridlab/gat/io/permissions/AbstractBasicFileAttributeView.java
@@ -12,11 +12,12 @@ import org.gridlab.gat.io.permissions.attribute.BasicFileAttributes;
 /**
  * @author Jerome Revillar
  */
-public abstract class AbstractBasicFileAttributeView implements BasicFileAttributeView{
-	protected URI location;
-	protected boolean followSymbolicLinks;
-	
-	private static final String SIZE_NAME = "size";
+public abstract class AbstractBasicFileAttributeView implements
+        BasicFileAttributeView {
+    protected URI location;
+    protected boolean followSymbolicLinks;
+
+    private static final String SIZE_NAME = "size";
     private static final String CREATION_TIME_NAME = "creationTime";
     private static final String LAST_ACCESS_TIME_NAME = "lastAccessTime";
     private static final String LAST_MODIFIED_TIME_NAME = "lastModifiedTime";
@@ -28,15 +29,16 @@ public abstract class AbstractBasicFileAttributeView implements BasicFileAttribu
     private static final String IS_SYMBOLIC_LINK_NAME = "isSymbolicLink";
     private static final String IS_OTHER_NAME = "isOther";
 
-	public AbstractBasicFileAttributeView(URI location, boolean followSymbolicLinks) {
-		this.location = location;
-		this.followSymbolicLinks = followSymbolicLinks;
-	}
-	
-	public String name() {
-		return "basic";
-	}
-	
+    public AbstractBasicFileAttributeView(URI location,
+            boolean followSymbolicLinks) {
+        this.location = location;
+        this.followSymbolicLinks = followSymbolicLinks;
+    }
+
+    public String name() {
+        return "basic";
+    }
+
     public Object getAttribute(String attribute) throws IOException {
         if (attribute.equals(SIZE_NAME))
             return readAttributes().size();
@@ -62,45 +64,48 @@ public abstract class AbstractBasicFileAttributeView implements BasicFileAttribu
             return readAttributes().isOther();
         return null;
     }
-    
+
     private Long checkTimeValue(Object value) {
         if (value == null)
             throw new NullPointerException();
-        Long time = (Long)value;
+        Long time = (Long) value;
         if (time < 0L && time != -1L)
             throw new IllegalArgumentException("time value cannot be negative");
         return time;
     }
 
-    public void setAttribute(String attribute, Object value) throws IOException{
-	    if (attribute.equals(LAST_MODIFIED_TIME_NAME)) {
-	        setTimes(checkTimeValue(value), null, null, TimeUnit.MILLISECONDS);
-	        return;
-	    }
-	    if (attribute.equals(LAST_ACCESS_TIME_NAME)) {
-	        setTimes(null, checkTimeValue(value), null, TimeUnit.MILLISECONDS);
-	        return;
-	    }
-	    if (attribute.equals(CREATION_TIME_NAME)) {
-	        setTimes(null, null, checkTimeValue(value), TimeUnit.MILLISECONDS);
-	        return;
-	    }
-	    throw new UnsupportedOperationException("'" + attribute + "' is unknown or read-only attribute");
-	}
+    public void setAttribute(String attribute, Object value) throws IOException {
+        if (attribute.equals(LAST_MODIFIED_TIME_NAME)) {
+            setTimes(checkTimeValue(value), null, null, TimeUnit.MILLISECONDS);
+            return;
+        }
+        if (attribute.equals(LAST_ACCESS_TIME_NAME)) {
+            setTimes(null, checkTimeValue(value), null, TimeUnit.MILLISECONDS);
+            return;
+        }
+        if (attribute.equals(CREATION_TIME_NAME)) {
+            setTimes(null, null, checkTimeValue(value), TimeUnit.MILLISECONDS);
+            return;
+        }
+        throw new UnsupportedOperationException("'" + attribute
+                + "' is unknown or read-only attribute");
+    }
 
-    protected Map<String, Object> getAttributesMap(BasicFileAttributes basicAttributes){
-		Map<String, Object> attributes = new HashMap<String, Object>();
-		attributes.put(SIZE_NAME, basicAttributes.size());
-		attributes.put(CREATION_TIME_NAME, basicAttributes.creationTime());
-		attributes.put(LAST_ACCESS_TIME_NAME, basicAttributes.lastAccessTime());
-		attributes.put(LAST_MODIFIED_TIME_NAME, basicAttributes.lastModifiedTime());
-		attributes.put(RESOLUTION_NAME, basicAttributes.resolution());
-		attributes.put(FILE_KEY_NAME, basicAttributes.fileKey());
-		attributes.put(LINK_COUNT_NAME, basicAttributes.linkCount());
-		attributes.put(IS_DIRECTORY_NAME, basicAttributes.isDirectory());
-		attributes.put(IS_REGULAR_FILE_NAME, basicAttributes.isRegularFile());
-		attributes.put(IS_SYMBOLIC_LINK_NAME, basicAttributes.isSymbolicLink());
-		attributes.put(IS_OTHER_NAME, basicAttributes.isOther());
-		return attributes;
-	}
+    protected Map<String, Object> getAttributesMap(
+            BasicFileAttributes basicAttributes) {
+        Map<String, Object> attributes = new HashMap<String, Object>();
+        attributes.put(SIZE_NAME, basicAttributes.size());
+        attributes.put(CREATION_TIME_NAME, basicAttributes.creationTime());
+        attributes.put(LAST_ACCESS_TIME_NAME, basicAttributes.lastAccessTime());
+        attributes.put(LAST_MODIFIED_TIME_NAME, basicAttributes
+                .lastModifiedTime());
+        attributes.put(RESOLUTION_NAME, basicAttributes.resolution());
+        attributes.put(FILE_KEY_NAME, basicAttributes.fileKey());
+        attributes.put(LINK_COUNT_NAME, basicAttributes.linkCount());
+        attributes.put(IS_DIRECTORY_NAME, basicAttributes.isDirectory());
+        attributes.put(IS_REGULAR_FILE_NAME, basicAttributes.isRegularFile());
+        attributes.put(IS_SYMBOLIC_LINK_NAME, basicAttributes.isSymbolicLink());
+        attributes.put(IS_OTHER_NAME, basicAttributes.isOther());
+        return attributes;
+    }
 }
--- api/src/org/gridlab/gat/io/permissions/AbstractPosixFileAttributeView.java
+++ api/src/org/gridlab/gat/io/permissions/AbstractPosixFileAttributeView.java
@@ -12,63 +12,66 @@ import org.gridlab.gat.io.permissions.attribute.*;
 /**
  * @author Jerome Revillard
  */
-public abstract class AbstractPosixFileAttributeView extends AbstractBasicFileAttributeView implements PosixFileAttributeView {
-	private static final String PERMISSIONS_NAME = "permissions"; // Set<PosixFilePermission>
-	private static final String OWNER_NAME = "owner"; // UserPrincipal
-	private static final String GROUP_NAME = "group"; // GroupPrincipal
+public abstract class AbstractPosixFileAttributeView extends
+        AbstractBasicFileAttributeView implements PosixFileAttributeView {
+    private static final String PERMISSIONS_NAME = "permissions"; // Set<PosixFilePermission>
+    private static final String OWNER_NAME = "owner"; // UserPrincipal
+    private static final String GROUP_NAME = "group"; // GroupPrincipal
 
-	public AbstractPosixFileAttributeView(URI location, boolean followSymbolicLinks) {
-		super(location, followSymbolicLinks);
-	}
+    public AbstractPosixFileAttributeView(URI location,
+            boolean followSymbolicLinks) {
+        super(location, followSymbolicLinks);
+    }
 
-	public String name() {
-		return "posix";
-	}
+    public String name() {
+        return "posix";
+    }
 
-	@Override
-	public Object getAttribute(String attribute) throws IOException {
-		Object attributeValue = super.getAttribute(attribute);
-		if (attributeValue == null) {
-			if (attribute.equals(PERMISSIONS_NAME))
-				attributeValue = readAttributes().permissions();
-			if (attribute.equals(OWNER_NAME))
-				attributeValue = readAttributes().owner();
-			if (attribute.equals(GROUP_NAME))
-				attributeValue = readAttributes().group();
-		}
-		return attributeValue;
-	}
+    @Override
+    public Object getAttribute(String attribute) throws IOException {
+        Object attributeValue = super.getAttribute(attribute);
+        if (attributeValue == null) {
+            if (attribute.equals(PERMISSIONS_NAME))
+                attributeValue = readAttributes().permissions();
+            if (attribute.equals(OWNER_NAME))
+                attributeValue = readAttributes().owner();
+            if (attribute.equals(GROUP_NAME))
+                attributeValue = readAttributes().group();
+        }
+        return attributeValue;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setAttribute(String attribute, Object value) throws IOException {
-		if (attribute.equals(PERMISSIONS_NAME)) {
-			setPermissions((Set<PosixFilePermission>) value);
-			return;
-		}
-		if (attribute.equals(OWNER_NAME)) {
-			setOwner((UserPrincipal) value);
-			return;
-		}
-		if (attribute.equals(GROUP_NAME)) {
-			setGroup((GroupPrincipal) value);
-			return;
-		}
-		super.setAttribute(attribute, value);
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setAttribute(String attribute, Object value) throws IOException {
+        if (attribute.equals(PERMISSIONS_NAME)) {
+            setPermissions((Set<PosixFilePermission>) value);
+            return;
+        }
+        if (attribute.equals(OWNER_NAME)) {
+            setOwner((UserPrincipal) value);
+            return;
+        }
+        if (attribute.equals(GROUP_NAME)) {
+            setGroup((GroupPrincipal) value);
+            return;
+        }
+        super.setAttribute(attribute, value);
+    }
 
-	public UserPrincipal getOwner() throws IOException {
-		return (UserPrincipal) getAttribute(OWNER_NAME);
-	}
-	
-	public Map<String, ?> readAttributes(String first, String... rest) throws IOException {
-		Set<String> requestedAttributesNames = new HashSet<String>();
-		boolean copyAll = false;
-		if (first.equals("*")) {
+    public UserPrincipal getOwner() throws IOException {
+        return (UserPrincipal) getAttribute(OWNER_NAME);
+    }
+
+    public Map<String, ?> readAttributes(String first, String... rest)
+            throws IOException {
+        Set<String> requestedAttributesNames = new HashSet<String>();
+        boolean copyAll = false;
+        if (first.equals("*")) {
             copyAll = true;
         } else {
-        	requestedAttributesNames.add(first);
-            for (String attribute: rest) {
+            requestedAttributesNames.add(first);
+            for (String attribute : rest) {
                 if (attribute.equals("*")) {
                     copyAll = true;
                     break;
@@ -76,27 +79,30 @@ public abstract class AbstractPosixFileAttributeView extends AbstractBasicFileAt
                 requestedAttributesNames.add(attribute);
             }
         }
-		
-		PosixFileAttributes posixFileAttributes = readAttributes();
-		Map<String, Object> attributes = getAttributesMap(posixFileAttributes);
-		if(copyAll){
-			return attributes;
-		}
-		
-		for (Iterator<String> iterator = attributes.keySet().iterator(); iterator.hasNext();) {
-			String attributeName = iterator.next();
-			if(!requestedAttributesNames.contains(attributeName)){
-				iterator.remove();
-			}
-		}
-		return attributes;
-	}
 
-	protected Map<String, Object> getAttributesMap(PosixFileAttributes posixFileAttributes) {
-		Map<String, Object> attributes = super.getAttributesMap(posixFileAttributes);
-		attributes.put(PERMISSIONS_NAME, posixFileAttributes.permissions());
-		attributes.put(OWNER_NAME, posixFileAttributes.owner());
-		attributes.put(GROUP_NAME, posixFileAttributes.group());
-		return attributes;
-	}
+        PosixFileAttributes posixFileAttributes = readAttributes();
+        Map<String, Object> attributes = getAttributesMap(posixFileAttributes);
+        if (copyAll) {
+            return attributes;
+        }
+
+        for (Iterator<String> iterator = attributes.keySet().iterator(); iterator
+                .hasNext();) {
+            String attributeName = iterator.next();
+            if (!requestedAttributesNames.contains(attributeName)) {
+                iterator.remove();
+            }
+        }
+        return attributes;
+    }
+
+    protected Map<String, Object> getAttributesMap(
+            PosixFileAttributes posixFileAttributes) {
+        Map<String, Object> attributes = super
+                .getAttributesMap(posixFileAttributes);
+        attributes.put(PERMISSIONS_NAME, posixFileAttributes.permissions());
+        attributes.put(OWNER_NAME, posixFileAttributes.owner());
+        attributes.put(GROUP_NAME, posixFileAttributes.group());
+        return attributes;
+    }
 }
