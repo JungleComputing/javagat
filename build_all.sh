e8eed52c5ddbdfe1be13816d959d90977da0e625
Bas Boterman <bbn230@few.vu.nl>
2009-05-13 12:48:02 +0000

    Added/modified build scripts.

--- build_all.sh
+++ build_all.sh
@@ -2,10 +2,13 @@
 
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
 cd ../advert-lib/.
+pwd
 ant
-cp lib/* ../app-engine/adaptors/AppEngine/external/.
+cp -v lib/* ../app-engine/adaptors/AppEngine/external/.
 cd ../app-engine/.
+pwd
 ant
-cp lib/adaptors/AppEngineAdaptor/*.jar lib/.
-rm lib/AppEngineAdaptor.jar
-./bin/run-gat-app examples20.EndpointExample http://bbn230.appspot.com/ ibisappengine@gmail.com k9chaBay local
+#cp -v lib/adaptors/AppEngineAdaptor/*.jar lib/.
+#rm -v lib/AppEngineAdaptor.jar
+chmod -v 700 run_all.sh
+./run_all.sh
