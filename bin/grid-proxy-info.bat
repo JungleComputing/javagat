1883978722c13804106ca99bab2aaaefdd42581e
Giuseppe Nicoletti <nicoletti.g@gmail.com>
2009-06-02 23:08:18 +0000

    prova serale

--- bin/grid-proxy-info.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-FOR %%i IN ("%GAT_LOCATION%\lib\adaptors\shared\*.jar") DO CALL "%GAT_LOCATION%\bin\AddToGATClassPath.bat" %%i
-java -DUID="%USERNAME%" -classpath "%GAT_LOCATION%\lib\adaptors\GlobusAdaptor\cog-jglobus.jar";"%GAT_CLASSPATH%" org.globus.tools.ProxyInfo %*
