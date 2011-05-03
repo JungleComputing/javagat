27ece7bc10689d49ce0cfed5207cc459212e238b
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2011-05-03 08:59:03 +0000

    Reduce number of jars in lib directory by combining when possible

--- bin/grid-proxy-info.bat
+++ bin/grid-proxy-info.bat
@@ -2,4 +2,4 @@
 
 IF "%GAT_LOCATION%X"=="X" set GAT_LOCATION=.
 FOR %%i IN ("%GAT_LOCATION%\lib\adaptors\shared\*.jar") DO CALL "%GAT_LOCATION%\bin\AddToGATClassPath.bat" %%i
-java -DUID="%USERNAME%" -classpath "%GAT_LOCATION%\lib\adaptors\GlobusAdaptor\cog-jglobus.jar";"%GAT_CLASSPATH%" org.globus.tools.ProxyInfo %*
+java -DUID="%USERNAME%" -classpath "%GAT_LOCATION%\lib\adaptors\GlobusAdaptor\GlobusAdaptor.jar";"%GAT_CLASSPATH%" org.globus.tools.ProxyInfo %*
