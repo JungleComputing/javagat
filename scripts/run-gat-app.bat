8a3555d2f3a454e69454600a17e75e72c63714c3
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2011-12-19 11:03:58 +0000

    Moved bin to scripts

--- scripts/run-gat-app.bat
+++ scripts/run-gat-app.bat
@@ -11,6 +11,6 @@ set GAT_ADAPTOR_LIB_LOCATION=%GAT_LIB_LOCATION%\adaptors
 :: Create the path with the JAR files
 SET GAT_CLASSPATH=
 
-FOR %%i IN ("%GAT_LIB_LOCATION%\*.jar") DO CALL "%GAT_LOCATION%\bin\AddToGATClassPath.bat" %%i
+FOR %%i IN ("%GAT_LIB_LOCATION%\*.jar") DO CALL "%GAT_LOCATION%\scripts\AddToGATClassPath.bat" %%i
 
 java -cp "%CLASSPATH%";"%GAT_CLASSPATH%" -Dlog4j.configuration=file:"%GAT_LOCATION%"\log4j.properties -Dgat.adaptor.path="%GAT_ADAPTOR_LIB_LOCATION%" %*
